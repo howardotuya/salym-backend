86e38dad8000571ea779aea50dcd4913e74ce195
howard otuya <howardotuya17@gmail.com>
2023-05-16 22:45:07 +0100

    Edited Cors

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 require('dotenv').config({ path: "./config.env"});
 const errorHandler = require("./middleware/errorHandler");
 const express = require('express');
-const cors = require('cors');
 
-const allowedorigins = ['http://localhost:3000', 'https://salym-wellness-center-cqv3pqnyj-jaulf.vercel.app/' , 'http://localhost:5000', 'https://salym-wellness-center.vercel.app/', 'https://salymwellness.center/', 'https://www.salymwellness.center/'];
 const app = express();
 
 app.use(express.json());
-app.use(cors({ 
-    origin: allowedorigins
-})) 
 
 app.use("/api/mail", require('./routes/mail'));
 
